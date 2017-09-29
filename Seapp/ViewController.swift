1978821d3405a362a826385a08df5f284eb7b8b6
Edward Nguyen <edward.nguyen9@gmail.com>
2017-09-29 10:21:33 +0300

    Initial Commit

---
+++ Seapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Seapp
+//
+//  Created by Edward Nguyễn on 29/09/2017.
+//  Copyright © 2017 Edward Nguyễn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
